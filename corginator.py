311745147975dbf2972fcf6f04a762249f3c245c
André Kato <andrekato@usp.br>
2018-08-20 16:56:33 -0300

    Updated corginator.py

--- corginator.py
+++ corginator.py
@@ -1,29 +1,59 @@
-import tweepy
-import time
-from random import randint
+import os
 from credentials import *
+from random import choice
+from random import randint
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import StreamListener
+from tweepy import Stream
+from tweepy import API
+from tweepy import Cursor
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret) 
+api = API(auth)
 
-auth.set_access_token(access_token, access_secret)
-
-api = tweepy.API(auth)
-
-greetings = ['Did someone ask for a Corgi picture?', 'Here\'s a Corgi to liven up your day.', 'Corgis are awesome, aren\'t they? No wonder the Queen of England loves them.']
+for follower in Cursor(api.followers).items():
+    follower.follow()
+    print("Followed back: {}".format(follower.screen_name))
 
 def send_corgi(username, status_id):
-	greeting_id = randint(0, len(greetings) - 1)
-	api.update_with_media('corgi.png', status = '@{} {}'.format(username, greetings[greeting_id]), in_reply_to_status_id = status_id)
+	greetings = ['Did someone ask for a Corgi picture?', 'Here\'s a Corgi to liven up your day.', 'I hope you\'re having a great day. Here, have a Corgi.', 'What do you call a Corgi with three eyes? A Corgiii.', 'Why can\'t Corgis bring in the round at the pub? Because they are a bit short.', 'What did one Corgi say to another? \"Woof\".', 'What do you call a Corgi out of cash? A Pembroke!']
+	image_path = "corgis/"
+	number_images = sum([len(files) for r, d, files in os.walk("C:/Users/Andre/Desktop/TheCorginator/corgis")])
+	image_path += "{}.jpeg".format(randint(1, number_images))
+	api.update_with_media(image_path, status = '@{} {}'.format(username, choice(greetings)), in_reply_to_status_id = status_id)
 
-
-for follower in tweepy.Cursor(api.followers).items():
-	follower.follow()
-
-class BotStreamer(tweepy.MyStreamListener):
+class Listener(StreamListener):
 	def on_status(self, status):
 		username = status.user.screen_name
-		status_id = status.status_id
+		status_id = status.id
+		print("Received tweet.")
+		send_corgi(username, status_id)
 
-myStreamListener = BotStreamer()
-myStream = tweepy.Stream(auth, myStreamListener)
-myStream.filter(track=['@corginatorbot'])
+	def on_error(self, status):
+		if status == 420:
+			print("Whoops. 420 detected. Returning false.")
+			return False
+		print(status)
+
+class Authenticator:
+	def authenticate(self):
+		auth = OAuthHandler(consumer_key, consumer_secret)
+		auth.set_access_token(access_token, access_secret)
+		return auth
+
+class TwitterStreamer:
+	def __init__(self):
+		self.twitter_authenticator = Authenticator()
+
+	def stream_tweets(self, wordlist):
+		listener = Listener()
+		auth = self.twitter_authenticator.authenticate()
+		stream = Stream(auth, listener)
+		stream.filter(track=wordlist)
+
+if __name__ == "__main__":
+	wordlist = ["@CorginatorBot"]
+	streamer = TwitterStreamer()
+	streamer.stream_tweets(wordlist)
